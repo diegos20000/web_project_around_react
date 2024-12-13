80720009424b0fd091dd13f4b8341fe6da710e38
Diego-Rodrigo-Cano <dsuccess2020@gmail.com>
2024-12-13 11:11:00 -0500

    sprint 14 50%

--- main.jsx
+++ main.jsx
@@ -3,7 +3,6 @@ import App from "./src/components/App.jsx";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
